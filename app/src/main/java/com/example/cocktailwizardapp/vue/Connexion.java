e96dc9c81f10edf2da0011b01ed31700972fe7c4
Hesus47 <pablo.coroa@hotmail.com>
2024-03-22 16:41:53 -0400

    Connexion?

--- app/src/main/java/com/example/cocktailwizardapp/vue/Connexion.java
+++ app/src/main/java/com/example/cocktailwizardapp/vue/Connexion.java
@@ -8,13 +8,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-;import com.example.cocktailwizardapp.R;
+import android.widget.Toast;
+import com.example.cocktailwizardapp.R;
+import com.example.cocktailwizardapp.classes.ApiCommunication;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class Connexion extends AppCompatActivity implements View.OnClickListener {
 
     TextView lienVersInscription;
     EditText inputNomCon,inputMdpCon;
     Button btnConnexion;
+    String nom,mdp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +46,34 @@ public class Connexion extends AppCompatActivity implements View.OnClickListener
         if(v == lienVersInscription){
             Intent lienInscription = new Intent(this, Inscription.class);
             startActivity(lienInscription);
+        } else if (v == btnConnexion) {
+            ApiCommunication conn = new ApiCommunication();
+            Callback callback = new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    //TODO
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    //TODO
+                }
+            };
+            nom = inputNomCon.getText().toString();
+            mdp = inputMdpCon.getText().toString();
+            conn.connexion(nom,mdp,callback);
+
+            //TODO
+            // SharedPreference pour maintenire la connection
+            // If reponse succes!
+            /*if(good){
+            Intent galerie = new Intent(this, Galerie.class);
+            startActivity(galerie);
+            }
+            else{
+            Toast.makeText(this, "Nom ou mot de passe invalide", Toast.LENGTH_SHORT).show();
+            }
+             */
         }
     }
 }
