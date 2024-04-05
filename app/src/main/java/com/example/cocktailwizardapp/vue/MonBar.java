b26253504be88907af0a93380eb33565a8df79f1
Hesus47 <pablo.coroa@hotmail.com>
2024-04-05 14:17:59 -0400

    initial commit Mon bar moblie
    - Creation de l'activite monbar
    - Creation du dialog pour le dropdown

---
+++ app/src/main/java/com/example/cocktailwizardapp/vue/MonBar.java
@@ -0,0 +1,16 @@
+package com.example.cocktailwizardapp.vue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cocktailwizardapp.R;
+
+public class MonBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mon_bar);
+    }
+}
